9a476da993b409ee979e2fec6aa978f74c0619d2
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-05 11:39:18 +0300

    Added validation constants in a Common Class library project

---
+++ TimeForge.Common/Constants/ProjectValidationConstants.cs
@@ -0,0 +1,7 @@
+namespace TimeForge.Common.Constants;
+
+public static class ProjectValidationConstants
+{
+    public const int NameMaxLength = 100;
+    public const int NameMinLength = 3;
+}
---
+++ TimeForge.Common/Constants/TaskValidationConstants.cs
@@ -0,0 +1,8 @@
+namespace TimeForge.Common.Constants;
+
+public static class TaskValidationConstants
+{
+    public const int NameMaxLength = 150;
+
+    public const int NameMinLength = 5;
+}
