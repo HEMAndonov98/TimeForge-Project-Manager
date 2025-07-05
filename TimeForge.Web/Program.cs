a324d51fa5355fa9193f7ea6ec7f3f7efccede68
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-05 12:14:56 +0300

    Scaffolded Identity

--- TimeForge.Web/Program.cs
+++ TimeForge.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TimeForge.Infrastructure;
 using TimeForge.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
