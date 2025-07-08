6ed8e1769abe9ded149146f621c5cee7715b4d68
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-08 09:54:12 +0300

    updates to csproj and simillar

---
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -0,0 +1,10 @@
+namespace TimeForge.Common;
+
+public class TasksToCompleteInTheNextCodingSession
+{
+    //TODO Add query interceptor for safe deletion next time
+
+    //TODO Implement repository pattern
+    
+    //TODO Start work on Services
+}
