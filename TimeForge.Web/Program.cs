469e3f5635ec4a7b47b04da5556e193f7e270be2
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-08 09:51:56 +0300

    Fixed dbContext provider to use SqlServer

--- TimeForge.Web/Program.cs
+++ TimeForge.Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<TimeForgeDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
