145c0389374d6b70326950d5cd32a879f14523ed
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-08 22:10:03 +0300

    csproj update

--- TimeForge.Infrastructure/Repositories/TimeForgeRepository.cs
+++ TimeForge.Infrastructure/Repositories/TimeForgeRepository.cs
@@ -1,8 +1,9 @@
 using TimeForge.Infrastructure.Repositories.Common;
+using TimeForge.Infrastructure.Repositories.Interfaces;
 
 namespace TimeForge.Infrastructure.Repositories;
 
-public class TimeForgeRepository(TimeForgeDbContext context) : BaseRepository<TimeForgeDbContext>(context)
+public class TimeForgeRepository(TimeForgeDbContext context) :  BaseRepository<TimeForgeDbContext>(context), ITimeForgeRepository
 {
     //TODO special case operations go here 
 }
