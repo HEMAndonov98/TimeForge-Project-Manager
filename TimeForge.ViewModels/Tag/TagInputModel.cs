5f94f518d77685634aae443353c9c8529cec3221
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-15 21:45:46 +0300

    **feat:** define `TagInputModel` with validation attributes and default values, and create `TagViewModel` for presenting tag detail

--- TimeForge.ViewModels/Tag/TagInputModel.cs
+++ TimeForge.ViewModels/Tag/TagInputModel.cs
@@ -5,6 +5,8 @@ namespace TimeForge.ViewModels.Tag;
 
 public class TagInputModel
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    
     [Required(ErrorMessage = TagDtoValidationConstants.TagNameRequiredErrorMessage)]
     [StringLength(TagDtoValidationConstants.TagNameMaxLength,
         MinimumLength = TagDtoValidationConstants.TagNameMinLength,
@@ -12,4 +14,6 @@ public class TagInputModel
     public string Name { get; set; } = null!;
     
     public string UserId { get; set; } = null!;
+
+    public string ProjectId { get; set; } = null!;
 }
--- TimeForge.ViewModels/Tag/TagViewModel.cs
+++ TimeForge.ViewModels/Tag/TagViewModel.cs
@@ -3,6 +3,5 @@ namespace TimeForge.ViewModels.Tag;
 public class TagViewModel
 {
     public string Id { get; set; }
-
     public string Name { get; set; }
 }
