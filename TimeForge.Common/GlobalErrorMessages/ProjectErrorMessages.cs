d18efbab60baf936a008e4536f52ff75b2f657f1
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-15 21:43:08 +0300

    Added Project Error Messages Common

---
+++ TimeForge.Common/GlobalErrorMessages/ProjectErrorMessages.cs
@@ -0,0 +1,8 @@
+namespace TimeForge.Common.GlobalErrorMessages;
+
+public static class ProjectErrorMessages
+{
+    public const string ProjectNameRequired = "Project name is required";
+
+    public const string ProjectNameLength = "Project name must be between {2} and {1} characters long";
+}
