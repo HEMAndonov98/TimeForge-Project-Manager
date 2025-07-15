0dabc4fe9606ad719e925e634e474e61534e4934
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-15 21:34:33 +0300

    Enhance home page with project listing and user-specific data
    - Implemented `Index` action in `HomeController` to load projects specific to the authenticated user.
    - Designed `Index.cshtml` to display a dynamic home page:
      - Welcome banner for unauthenticated users with login and registration options.
      - Project listing for authenticated users, including project details, tags, and a link to view more details.
      - Displayed a call-to-action for users without active projects to create a new one.
    - Integrated responsive and user-friendly UI elements using Bootstrap for a polished experience.

--- TimeForge.Web/Controllers/HomeController.cs
+++ TimeForge.Web/Controllers/HomeController.cs
@@ -1,22 +1,37 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using TimeForge.Models;
+using TimeForge.Services.Interfaces;
+using TimeForge.ViewModels.Project;
 using TimeForge.ViewModels.Shared;
 
 namespace TimeForge.Web.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IProjectService projectService;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IProjectService projectService)
     {
         _logger = logger;
+        this.projectService = projectService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var user = this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var isLoggedIn = User.Identity?.IsAuthenticated ?? false;
+        this.ViewData["IsLoggedIn"] = isLoggedIn;
+        
+        var projects = new List<ProjectViewModel>();
+        if (!string.IsNullOrEmpty(user))
+        {
+            var data = await this.projectService.GetAllProjectsAsync(user);
+            projects = data.ToList();
+        }
+        return View("Index", projects);
     }
 
     public IActionResult Privacy()
