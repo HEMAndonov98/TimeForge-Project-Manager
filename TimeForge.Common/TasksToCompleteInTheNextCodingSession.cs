33a6f3be806368716e27884b47d54afb39c9d0b5
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-19 17:14:04 +0300

    added new tasks to complete

--- TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -2,11 +2,12 @@ namespace TimeForge.Common;
 
 public class TasksToCompleteInTheNextCodingSession
 {
-    //TODO Add query interceptor for safe deletion next time
     
     //TODO Implement Soft delete of projects
     
     //TODO Check how and If tags are sent with project input model in project controller
+    
+    //TODO Add controlers and methods for Index.cshtml in Areas/Manager/Views
     
     //Test user
     //Email: testUser@testUser.com
