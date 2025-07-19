1ef8df2d4c7f038eca9d25af2936eef92f084afc
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-19 14:36:02 +0300

    Changed tasks for TasksToCompleteInTheNextCodingSession.cs

--- TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -3,11 +3,10 @@ namespace TimeForge.Common;
 public class TasksToCompleteInTheNextCodingSession
 {
     //TODO Add query interceptor for safe deletion next time
-
-    //TODO Implement repository pattern
     
-    //TODO Start work on Services
+    //TODO Implement Soft delete of projects
     
+    //TODO Check how and If tags are sent with project input model in project controller
     
     //Test user
     //Email: testUser@testUser.com
