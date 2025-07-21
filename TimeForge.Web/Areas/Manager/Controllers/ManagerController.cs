277800a50abeb1c7d247113d6ad07a54d6e42c3a
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-21 17:43:49 +0300

    Removed Authorize from ManagerController.cs

--- TimeForge.Web/Areas/Manager/Controllers/ManagerController.cs
+++ TimeForge.Web/Areas/Manager/Controllers/ManagerController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace TimeForge.Web.Areas.Manager.Controllers;
 
 [Area("Manager")]
-[Authorize(Roles = "Manager")]
 public class ManagerController : Controller
 {
     // GET
