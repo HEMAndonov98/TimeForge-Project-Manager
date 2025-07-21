9fdeb99adc7eadf79e6ad072886d27d1f70f5699
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-21 17:46:29 +0300

    Added Create POST method for TaskController.cs

--- TimeForge.Web/Controllers/TaskController.cs
+++ TimeForge.Web/Controllers/TaskController.cs
@@ -1,7 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using TimeForge.Services.Interfaces;
+using TimeForge.ViewModels.Task;
 
 namespace TimeForge.Web.Controllers;
 
 public class TaskController : Controller
 {
+    
+    private readonly ITaskService taskService;
+    private readonly ILogger<TaskController> logger;
+
+    public TaskController(ITaskService taskService, ILogger<TaskController> logger)
+    {
+        this.taskService = taskService;
+        this.logger = logger;
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(TaskInputModel inputModel)
+    {
+        this.logger.LogInformation("Creating task for project {ProjectId}", inputModel.ProjectId);
+        await this.taskService.CreateTaskAsync(inputModel);
+        return RedirectToAction("Details", "Project", new { projectId = inputModel.ProjectId });
+    }
 }
