3e5b8bbce69f14f26acbf75fc9a308b1f3aa6474
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-21 17:47:04 +0300

    TaskViewModel.cs feat: added Id property to view model

--- TimeForge.ViewModels/Task/TaskViewModel.cs
+++ TimeForge.ViewModels/Task/TaskViewModel.cs
@@ -4,6 +4,7 @@ namespace TimeForge.ViewModels.Task;
 
 public class TaskViewModel
 {
+    public string Id { get; set; }
     public string Name { get; set; }
 
     public bool IsCompleted { get; set; }
