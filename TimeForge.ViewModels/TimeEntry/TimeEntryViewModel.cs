d65882c32ba3b2ee4f717960b62ba7f210ccae45
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-21 17:47:59 +0300

    Added necessary properties to TimeEntryViewModel.cs

--- TimeForge.ViewModels/TimeEntry/TimeEntryViewModel.cs
+++ TimeForge.ViewModels/TimeEntry/TimeEntryViewModel.cs
@@ -2,5 +2,13 @@ namespace TimeForge.ViewModels.TimeEntry;
 
 public class TimeEntryViewModel
 {
-    
+    public DateTime Start { get; set; }
+
+    public DateTime? End { get; set; }
+
+    public string TaskId { get; set; }
+
+    public string CreatedBy { get; set; }
+
+    public bool IsRunning { get; set; }
 }
