c4f7b1636823916f31e83c04eefe0f31b8222153
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-21 17:46:41 +0300

    Cleared all tasks for now

--- TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -2,13 +2,6 @@ namespace TimeForge.Common;
 
 public class TasksToCompleteInTheNextCodingSession
 {
-    
-    //TODO Implement Soft delete of projects
-    
-    //TODO Check how and If tags are sent with project input model in project controller
-    
-    //TODO Add controlers and methods for Index.cshtml in Areas/Manager/Views
-    
     //Test user
     //Email: testUser@testUser.com
     //Passowrd: testUser123_
