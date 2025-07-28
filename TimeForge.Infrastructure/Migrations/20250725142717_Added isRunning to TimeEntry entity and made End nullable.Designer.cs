4e98bf04fe8fc3f780af7017fe621404e200214f
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-28 19:26:39 +0300

    Added new migration with additional properties

--- TimeForge.Infrastructure/Migrations/20250725142717_Added isRunning to TimeEntry entity and made End nullable.Designer.cs
+++ TimeForge.Infrastructure/Migrations/20250725142717_Added isRunning to TimeEntry entity and made End nullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimeForge.Infrastructure;
 
@@ -11,9 +12,11 @@ using TimeForge.Infrastructure;
 namespace TimeForge.Infrastructure.Migrations
 {
     [DbContext(typeof(TimeForgeDbContext))]
-    partial class TimeForgeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250725142717_Added isRunning to TimeEntry entity and made End nullable")]
+    partial class AddedisRunningtoTimeEntryentityandmadeEndnullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
