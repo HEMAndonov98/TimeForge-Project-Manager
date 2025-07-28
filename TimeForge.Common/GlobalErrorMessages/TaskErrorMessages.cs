16b07269f9d7534df803c0b834b3255f22363d45
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-28 19:24:11 +0300

    Added TaskErrorMessages.cs for TaskInputModel.cs

---
+++ TimeForge.Common/GlobalErrorMessages/TaskErrorMessages.cs
@@ -0,0 +1,9 @@
+namespace TimeForge.Common.GlobalErrorMessages;
+
+public static class TaskErrorMessages
+{
+    public const string TaskNameRequired = "Task name is required";
+
+    public const string TaskNameLength = "Task description must be between {2} and {1} characters long";
+
+}
