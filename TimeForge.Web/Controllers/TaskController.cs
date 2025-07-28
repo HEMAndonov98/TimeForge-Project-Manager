00d2146ec2424edf736be3a98f258d173d10aa05
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-28 19:23:53 +0300

    TaskController.cs has methods Complete and Create for respectively creating and completing tasks

--- TimeForge.Web/Controllers/TaskController.cs
+++ TimeForge.Web/Controllers/TaskController.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TimeForge.Services.Interfaces;
 using TimeForge.ViewModels.Task;
 
 namespace TimeForge.Web.Controllers;
-
+[Authorize]
 public class TaskController : Controller
 {
     
@@ -18,10 +19,43 @@ public class TaskController : Controller
     
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(TaskInputModel inputModel)
+    public async Task<IActionResult> Create(TaskListAndFormModel listAndFormModel)
     {
+        var inputModel = listAndFormModel.TaskInputModel;
+        
         this.logger.LogInformation("Creating task for project {ProjectId}", inputModel.ProjectId);
+
+        if (!ModelState.IsValid)
+        {
+            this.logger.LogWarning("Model state is invalid for Task with description: {TaskDescription}", inputModel.Name);
+            return RedirectToAction("Details", "Project", new { projectId = inputModel.ProjectId });
+        }
         await this.taskService.CreateTaskAsync(inputModel);
+        
+        this.logger.LogInformation("Task added for project {ProjectId}", inputModel.ProjectId);
         return RedirectToAction("Details", "Project", new { projectId = inputModel.ProjectId });
     }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Complete(string taskId)
+    {
+        await this.taskService.CompleteTask(taskId);
+
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTaskListPartial(string projectId)
+    {
+        var model = await this.taskService.GetTasksByProjectIdAsync(projectId);
+        var taskListAndFormModel = new TaskListAndFormModel()
+        {
+            Tasks = model.ToList(),
+            TaskInputModel = new TaskInputModel(),
+            ProjectId = projectId
+        };
+        return PartialView("_ProjectTaskList", taskListAndFormModel);
+    }
+
 }
