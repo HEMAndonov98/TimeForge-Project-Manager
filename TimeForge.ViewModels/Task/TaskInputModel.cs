c903c44e9d4e609803b1511675a3a1a6e42f7ab5
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-28 19:24:31 +0300

    added model validation for TaskInputModel.cs

--- TimeForge.ViewModels/Task/TaskInputModel.cs
+++ TimeForge.ViewModels/Task/TaskInputModel.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using TimeForge.Common.Constants;
+using TimeForge.Common.GlobalErrorMessages;
+
 namespace TimeForge.ViewModels.Task;
 
 public class TaskInputModel
 {
     public string ProjectId { get; set; } = null!;
+    [Required(ErrorMessage = TaskErrorMessages.TaskNameRequired)]
+    [StringLength(TaskValidationConstants.NameMaxLength,
+        MinimumLength = TaskValidationConstants.NameMinLength,
+        ErrorMessage = TaskErrorMessages.TaskNameLength)]
     public string Name { get; set; } = null!;
 }
