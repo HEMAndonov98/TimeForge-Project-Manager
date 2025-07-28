0a19bf000938516c221dce1e401d72fa996255c7
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-28 19:26:54 +0300

    New Tasks written for project

--- TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -2,6 +2,11 @@ namespace TimeForge.Common;
 
 public class TasksToCompleteInTheNextCodingSession
 {
+    //TODO 2.Continue work on adding tasks to projects check POST method if  data is sent in a valid state
+    //TODO 3.Follow task creation service to ensure tasks are being added correctly
+    
+    //TODO 4.Start work on TimerFunctionality
+    
     //Test user
     //Email: testUser@testUser.com
     //Passowrd: testUser123_
