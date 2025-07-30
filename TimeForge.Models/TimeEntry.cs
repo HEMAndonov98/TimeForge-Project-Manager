5fde7519358107b54750087e82b6b1316a8b1f9c
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-07-30 21:05:58 +0300

    Updated TimeEntry.cs domain model

--- TimeForge.Models/TimeEntry.cs
+++ TimeForge.Models/TimeEntry.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using TimeForge.Common.Enums;
 using TimeForge.Models.Common;
 
 namespace TimeForge.Models;
@@ -13,7 +14,37 @@ public class TimeEntry : BaseDeletableModel<string>
    public DateTime Start { get; set; }
 
    public DateTime? End { get; set; }
+
+   public TimeEntryState State { get; set; } = TimeEntryState.Running;
+
+   public DateTime? LastPausedAt { get; set; }
    
+   public TimeSpan TotalPausedDuration { get; set; } = TimeSpan.Zero;
+
+   [NotMapped]
+   public TimeSpan? Duration
+   {
+      get
+      {
+         if (End.HasValue)
+         {
+            return (End.Value - Start) - TotalPausedDuration;
+         }
+
+         if (State == TimeEntryState.Running && LastPausedAt.HasValue)
+         {
+            return (LastPausedAt.Value - Start) - TotalPausedDuration;
+         }
+
+         if (State == TimeEntryState.Running)
+         {
+            return (DateTime.UtcNow - Start) - TotalPausedDuration;
+         }
+
+         return null;
+      }
+   }
+
    [ForeignKey(nameof(ProjectTask))]
    public string TaskId { get; set; } = null!;
 
@@ -23,6 +54,4 @@ public class TimeEntry : BaseDeletableModel<string>
    public string UserId { get; set; } = null!;
 
    public User CreatedBy { get; set; } = null!;
-
-   public bool IsRunning { get; set; }
 }
