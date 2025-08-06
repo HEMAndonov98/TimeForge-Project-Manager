bb763565ff84a1b87ffb18c040912696b4192950
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-06 15:58:46 +0300

    Tasks updated

--- TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
+++ TimeForge.Common/TasksToCompleteInTheNextCodingSession.cs
@@ -2,7 +2,12 @@ namespace TimeForge.Common;
 
 public class TasksToCompleteInTheNextCodingSession
 {
-    //TODO 1. Start work on TimerFunctionality
+    //TimerWidget flow:
+    //Click Play -> Controller check if timer is running //TODO Implement Service method for checking if there are any running timers
+    //-> IF Yes Then resume Timer and redirectToAction("Index", "Home") ELSE Start timer in service redirectToAction("Index", "Home") ->
+    //Same with pause and stop //TODO Change return on TimeEntryController to return RedirectToAction("Index", "Home") on success.
+    
+    //Future plan is to use Ajax and SignalR to dynamically reload TimerWidget independently
     
     //TODO 2. Work on ManagerController(Assign Project to user)
     //TODO 3. Work on ManagerService(Assign Project to user)
