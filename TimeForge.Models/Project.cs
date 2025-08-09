80bf40fdd1054f6aedc674a54982f65b8f3dc08a
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-09 18:06:19 +0300

    Formatting

--- TimeForge.Models/Project.cs
+++ TimeForge.Models/Project.cs
@@ -12,50 +12,61 @@ namespace TimeForge.Models;
 public class Project : BaseDeletableModel<string>
 {
     /// <summary>
-/// Initializes a new instance of the <see cref="Project"/> class with a new unique identifier.
-/// </summary>
-public Project()
+    /// Initializes a new instance of the <see cref="Project"/> class with a new unique identifier.
+    /// </summary>
+    public Project()
     {
         this.Id = Guid.NewGuid().ToString();
     }
-    
+
+    /// <summary>
+    /// Gets or sets the name of the project.
+    /// </summary>
     [Required]
     [MaxLength(ProjectValidationConstants.NameMaxLength)]
-    /// <summary>
-/// Gets or sets the name of the project.
-/// </summary>
-public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
     /// <summary>
-/// Gets or sets a value indicating whether the project is public.
-/// </summary>
-public bool IsPublic { get; set; } = true;
+    /// Gets or sets a value indicating whether the project is public.
+    /// </summary>
+    public bool IsPublic { get; set; } = true;
 
     /// <summary>
-/// Gets or sets the due date of the project.
-/// </summary>
-public DateOnly? DueDate { get; set; }
+    /// Gets or sets the due date of the project.
+    /// </summary>
+    public DateOnly? DueDate { get; set; }
 
+    /// <summary>
+    /// Gets or sets the user ID of the project's creator.
+    /// </summary>
     [ForeignKey(nameof(CreatedBy))]
-    /// <summary>
-/// Gets or sets the user ID of the project's creator.
-/// </summary>
-public string UserId { get; set; } = null!;
+    public string UserId { get; set; } = null!;
 
     /// <summary>
-/// Gets or sets the user who created the project.
-/// </summary>
-public User CreatedBy { get; set; } = null!;
+    /// Gets or sets the user who created the project.
+    /// </summary>
+    public User CreatedBy { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets the user ID of the project's assigned user.
+    /// </summary>
+    [ForeignKey(nameof(AssignedTo))]
+    public string? AssignedUserId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user to whom the project is assigned.
+    /// </summary>
+    public User AssignedTo { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of tasks associated with the project.
+    /// </summary>
     [InverseProperty(nameof(ProjectTask.Project))]
-    /// <summary>
-/// Gets or sets the list of tasks associated with the project.
-/// </summary>
-public virtual List<ProjectTask> Tasks { get; set; } = new();
+    public virtual List<ProjectTask> Tasks { get; set; } = new();
 
-    [InverseProperty(nameof(ProjectTag.Project))]
     /// <summary>
-/// Gets or sets the list of tags associated with the project.
-/// </summary>
-public virtual List<ProjectTag> ProjectTags { get; set; } = new();
+    /// Gets or sets the list of tags associated with the project.
+    /// </summary>
+    [InverseProperty(nameof(ProjectTag.Project))]
+    public virtual List<ProjectTag> ProjectTags { get; set; } = new();
 }
