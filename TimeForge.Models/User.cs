d9bb803bdbd544e393673ed214f12096fa71f58d
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-09 18:09:25 +0300

    Formatting and created AssignedProjects prop for mapping

--- TimeForge.Models/User.cs
+++ TimeForge.Models/User.cs
@@ -9,43 +9,48 @@ namespace TimeForge.Models;
 public class User : IdentityUser
 {
     /// <summary>
-/// Gets or sets the list of projects created by the user.
-/// </summary>
-[InverseProperty(nameof(Project.CreatedBy))]
-public virtual List<Project> Projects { get; set; } = new();
+    /// Gets or sets the list of projects created by the user.
+    /// </summary>
+    [InverseProperty(nameof(Project.CreatedBy))]
+    public virtual List<Project> Projects { get; set; } = new();
 
     /// <summary>
-/// Gets or sets the list of tags created by the user.
-/// </summary>
-[InverseProperty(nameof(Tag.CreatedBy))]
-public virtual List<Tag> Tags { get; set; } = new();
+    /// Gets or sets the list of tags created by the user.
+    /// </summary>
+    [InverseProperty(nameof(Tag.CreatedBy))]
+    public virtual List<Tag> Tags { get; set; } = new();
 
     /// <summary>
-/// Gets or sets the list of time entries created by the user.
-/// </summary>
-[InverseProperty(nameof(TimeEntry.CreatedBy))]
-public virtual List<TimeEntry> TimeEntries { get; set; } = new();
-    
-    
-    //Manager properties
+    /// Gets or sets the list of time entries created by the user.
+    /// </summary>
+    [InverseProperty(nameof(TimeEntry.CreatedBy))]
+    public virtual List<TimeEntry> TimeEntries { get; set; } = new();
 
     /// <summary>
-/// Gets or sets a value indicating whether the user is a manager.
-/// </summary>
-public bool IsManager { get; set; }
+    /// Gets or sets the list of projects assigned to the user.
+    /// </summary>
+    [InverseProperty(nameof(Project.AssignedTo))]
+    public virtual List<Project> AssignedProjects { get; set; } = new();
+
+    // Manager properties
 
     /// <summary>
-/// Gets or sets the list of users managed by this user.
-/// </summary>
-public virtual List<User> ManagedUsers { get; set; } = new();
+    /// Gets or sets a value indicating whether the user is a manager.
+    /// </summary>
+    public bool IsManager { get; set; }
 
     /// <summary>
-/// Gets or sets the manager's user ID, if applicable.
-/// </summary>
-public string? ManagerId { get; set; } = null!;
+    /// Gets or sets the list of users managed by this user.
+    /// </summary>
+    public virtual List<User> ManagedUsers { get; set; } = new();
 
     /// <summary>
-/// Gets or sets the manager user entity, if applicable.
-/// </summary>
-public User? Manager { get; set; } = null!;
+    /// Gets or sets the manager's user ID, if applicable.
+    /// </summary>
+    public string? ManagerId { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the manager user entity, if applicable.
+    /// </summary>
+    public User? Manager { get; set; } = null!;
 }
