e5373ed0da23dcaa575b8261ccfcd501f01a8e1d
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-17 20:45:35 +0300

    Updated names on TaskCollectionConstants

---
+++ TimeForge.Common/Constants/TaskCollectionConstants.cs
@@ -0,0 +1,12 @@
+namespace TimeForge.Common.Constants;
+
+public static class TaskCollectionConstants
+{
+    public const int TaskItemDescriptionMaxLength = 100;
+    public const int TaskItemDescriptionMinLength = 3;
+    
+    
+    public const int TaskCollectionNameMaxLength = 100;
+    public const int TaskCollectionNameMinLength = 3;
+    
+}
