6f97d0d0d02794bf74bac3ad915e83352978e77c
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-17 18:36:08 +0300

    Updated BaseModel<Type> to include a maxLength for GUIDs as strings IDs

---
+++ TimeForge.Common/Constants/BaseModelConstants.cs
@@ -0,0 +1,6 @@
+namespace TimeForge.Common.Constants;
+
+public static class BaseModelConstants
+{
+    public const int IdGuidMaxLength = 40;
+}
--- TimeForge.Models/Common/BaseModel.cs
+++ TimeForge.Models/Common/BaseModel.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 
+using TimeForge.Common.Constants;
+
 namespace TimeForge.Models.Common;
 
 public abstract class BaseModel<TId>
 {
     [Key]
+    [MaxLength(BaseModelConstants.IdGuidMaxLength)]
     public TId Id { get; set; }
 }
