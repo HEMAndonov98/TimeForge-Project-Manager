bd23fbf070643a0defe832a27b97dc9763bd23ab
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-17 20:46:36 +0300

    Craeted error messages static class for TaskCollectionInputModel

---
+++ TimeForge.Common/Dto Validation/TaskCollectionDtoMessages.cs
@@ -0,0 +1,8 @@
+namespace TimeForge.Common.Dto_Validation;
+
+public static class TaskCollectionDtoMessages
+{
+    public const string TaskCollectionNameRequired = "Task collection title is required";
+    
+    public const string TaskCollectionNameLength = "Task collection title must be between {2} and {1} characters long";
+}
