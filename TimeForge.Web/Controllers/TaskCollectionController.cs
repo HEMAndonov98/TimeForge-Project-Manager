c1ed0ce6a701681aeed13d4941bc3d56cb2ace85
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-18 15:58:01 +0300

    TaskController methods for Index, Delete and Complete Task Implemented

--- TimeForge.Web/Controllers/TaskCollectionController.cs
+++ TimeForge.Web/Controllers/TaskCollectionController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +11,15 @@ namespace TimeForge.Web.Controllers;
 public class TaskCollectionController(ITaskCollectionService taskCollectionService) : Controller
 {
     private readonly ITaskCollectionService taskCollectionService = taskCollectionService;
-
+    //TODO Add xml documentation when finished implementing
+    //TODO add error handling
+    //TODO add logging
     [HttpGet]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        string userId = this.GetUserId();
+        var lists = await this.taskCollectionService.GetAllTaskCollectionsAsync(userId);
+        return View(lists);
     }
     
     
@@ -22,4 +28,61 @@ public class TaskCollectionController(ITaskCollectionService taskCollectionServi
     {
         return View(new TaskCollectionInputModel());
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(TaskCollectionInputModel inputModel)
+    {
+        inputModel.UserId = this.GetUserId();
+        if (!ModelState.IsValid)
+        {
+            return View(inputModel);
+        }
+
+        await this.taskCollectionService.CreateTaskCollectionAsync(inputModel);
+        return RedirectToAction("Index", "TaskCollection");
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddTaskItem(TaskItemInputModel inputModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        await this.taskCollectionService.CreateTaskItemAsync(inputModel);
+        return RedirectToAction("Index", "TaskCollection");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(string taskCollectionId)
+    {
+        try
+        {
+            var taskCollection = await this.taskCollectionService.GetTaskCollectionByIdAsync(taskCollectionId);
+            return PartialView("Delete", taskCollection);
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CompleteTaskItem(string taskId)
+    {
+        //TODO Implement CompletetaskItem
+        // await this.taskCollectionService.CompleteTaskItem
+        throw new NotImplementedException();
+    }
+    
+    /// <summary>
+    /// Gets the current user's ID from claims.
+    /// </summary>
+    /// <returns>The user ID as a string, or null if not found.</returns>
+    private string GetUserId()
+        => this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? String.Empty;
 }
