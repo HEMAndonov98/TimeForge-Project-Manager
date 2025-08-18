a18bdc01a458986a9f4924ab1c4fc72a22367c40
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-18 15:56:46 +0300

    TaskCollection audit and base properties self initialise on craete, also added reference to User

--- TimeForge.Models/TaskCollection.cs
+++ TimeForge.Models/TaskCollection.cs
@@ -7,10 +7,23 @@ namespace TimeForge.Models;
 
 public class TaskCollection : BaseDeletableModel<string>
 {
+    public TaskCollection()
+    {
+        this.Id = Guid.NewGuid().ToString();
+        this.CreatedAt = DateTime.UtcNow;
+        this.LastModified = DateTime.UtcNow;
+    }
+    
     [Required]
     [MaxLength(TaskCollectionConstants.TaskCollectionNameMaxLength)]
     public string ListName { get; set; } = null!;
 
+    
     [InverseProperty(nameof(TaskItem.TaskCollection))]
-    public List<TaskItem> ListTasks { get; set; } = new();
+    public List<TaskItem> TaskItems { get; set; } = new();
+
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; }
+
+    public User User { get; set; }
 }
