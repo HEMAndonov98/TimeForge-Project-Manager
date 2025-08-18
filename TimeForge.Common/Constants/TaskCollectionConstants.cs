bedda3106777fbd54df70ca16b82b6a0ed872f19
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-18 15:57:24 +0300

    Changed names for TaskItem constants

--- TimeForge.Common/Constants/TaskCollectionConstants.cs
+++ TimeForge.Common/Constants/TaskCollectionConstants.cs
@@ -2,8 +2,8 @@ namespace TimeForge.Common.Constants;
 
 public static class TaskCollectionConstants
 {
-    public const int TaskItemDescriptionMaxLength = 100;
-    public const int TaskItemDescriptionMinLength = 3;
+    public const int TaskItemTitleMaxLength = 100;
+    public const int TaskItemTitleMinLength = 3;
     
     
     public const int TaskCollectionNameMaxLength = 100;
