ac75b21dc0ff2c4bc0cd44a33e4ad1813037b051
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2026-02-15 20:51:43 +0200

    refactor: remove TimeForge.ViewModels project and add editor configuration files.

--- TimeForge.ViewModels/List/TaskCollectionViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeForge.ViewModels.List;
-
-public class TaskCollectionViewModel
-{
-    public string Id { get; set; }
-    public string Title { get; set; }
-
-    public List<TaskItemViewModel> Tasks { get; set; }
-}
--- TimeForge.ViewModels/Shared/ErrorViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace TimeForge.ViewModels.Shared;
-
-public class ErrorViewModel
-{
-    public string RequestId { get; set; }
-}
